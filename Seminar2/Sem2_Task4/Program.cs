0c1fb4b2fc5bb5404358bc8f62460f25b51567c9
Sanjar Shaketov <sanjar12129@gmail.com>
2024-10-04 23:09:28 +0500

    Sem2_Task4

---
+++ Seminar2/Sem2_Task4/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите прогрмму которая выводит третью слева цифра заданого числа или сообщает что третьей цифры нет.
+// Пример:
+// 456 => 6
+// 7812 => 1
+// 91 => третьей цифры нет
+
+
+
+
+Console.WriteLine("Введидите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num >= 100 || num <= -100)
+{
+    while(num > 999 || num < -999)
+    {
+        num = num / 10;
+    }
+    int result = num % 10;
+    Console.WriteLine("Третья цифра слева " + result);
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет ");
+}
