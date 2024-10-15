9713510f3e23d666c0977bd18e58fed6b4813bd7
Sanjar Shaketov <sanjar12129@gmail.com>
2024-10-15 21:59:51 +0500

    Sem3_Task4

---
+++ Seminar3/Sem3_Task4/Program.cs
@@ -0,0 +1,39 @@
+﻿// Данно натуральное трёхзначное число. Сщздайте массив, состоящий из цифр этого числа.
+// Младший разряд числа должен располагаться на 0-м индексе массива, старщий на 2-м.
+// пример: 
+// 456 => [6, 5, 4]
+// 781 => [1, 8, 7]
+
+
+
+Console.Write("Введите трёх значное число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int numCopy = num;
+int count = 0;
+
+// счетает колличество цифр в числе
+while(numCopy !=0)
+{
+    numCopy /= 10;
+    count++;
+}
+
+// 
+int[] arr = new int [count]; // создаем массив на колличество цифр
+int index = 0; // индекс последнего элемента
+while (num != 0)
+{
+    arr[index] = num % 10;
+    num /= 10; // num = num / 10;
+    index++;
+}
+Console.Write("[");
+for(int i =0; i < arr.Length; i++)
+{
+    Console.Write($"{arr[i]}");
+}
+Console.Write("]");
+
+
+
