997c38d72a38c855967d7dcafa267d245d122fd5
Sanjar Shaketov <sanjar12129@gmail.com>
2024-10-15 21:20:02 +0500

    Sem3_Task3

---
+++ Seminar3/Sem3_Task3/Program.cs
@@ -0,0 +1,28 @@
+﻿// Найдите произведение пар чисел в одномерном массиве. Парой читаемой первой и последний элемент,
+// второй и предпоследний и т.д. Результат запишите в новый массив.
+
+// [1,3,2,4,2,3] => [3,6,8]
+// [2,3,1,7,5,6,3] => [6,18,5] (Элемент 7 не имеет пары)
+
+
+
+
+int[] array = { 1, 3, 2, 4, 2, 3 };
+int[] arrayResult = new int[array.Length / 2];
+
+int left = 0;
+int right = array.Length - 1;
+
+while (left < right)
+{
+    arrayResult[left] = array[left] * array[right];
+    left++;
+    right--;
+}
+for (int i = 0; i < arrayResult.Length; i++)
+{
+    Console.Write(arrayResult[i] + " ");
+}
+
+
+
