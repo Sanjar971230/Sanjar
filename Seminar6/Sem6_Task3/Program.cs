1ef3d79219174c7382bd09c56a715cba0cf08ec2
Sanjar Shaketov <sanjar12129@gmail.com>
2024-11-14 22:03:27 +0500

    Sem6_Task3

---
+++ Seminar6/Sem6_Task3/Program.cs
@@ -0,0 +1,28 @@
+﻿// Считать строку с консоли, состоящую из латинских букв в нижнем регистре.
+// Выяснить, сколько среди введённых букв гласных.
+// Пример:
+// "hello" => 2
+// "world" => 1
+
+
+int CountVowels(string str)
+{
+    string vowels = "aeiouy";
+    int count = 0;
+    for(int i = 0; i < str.Length; i++)
+    {
+        for (int j = 0; j < vowels.Length; j++)
+        {
+            if (vowels[j] == str[i])
+            {
+                count++;
+            }
+        }
+    }
+    return count;
+}
+
+
+Console.WriteLine("Напиши слово: ");
+string start = Console.ReadLine();
+Console.WriteLine($"С слове {CountVowels(start)} гласных");
