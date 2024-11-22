02548eee5c1bbe567433262ebab6f73c3d29670d
Sanjar Shaketov <sanjar12129@gmail.com>
2024-11-22 21:02:46 +0500

    Sem7_Task3

---
+++ Seminar7/Sem7_Task3/Program.cs
@@ -0,0 +1,31 @@
+﻿// Считать строку с консоли, содержащую латинские буквы.
+// Вывести на экран согласные буквы этой строки.
+// Указание использовать рекурсию. Не использовать цикл.
+// Пример:
+// "hello" => hll
+// "World" => Wrld
+// "hello world!" => Hllwrld
+
+static void CheckPrintConsonats(string txt, int index)
+{
+string vowels = "aeiouy";
+if (char.IsAsciiLetter(txt[index]) && !vowels.Contains(txt[index]))
+{
+Console.Write($"{txt[index]}");
+}
+}
+
+void PrintConsonants(string txt, int index = 0)
+{
+if (index == txt.Length)
+{
+return;
+}
+CheckPrintConsonats(txt, index);
+PrintConsonants(txt, index + 1);
+}
+
+Console.WriteLine("введите строку содержащую латинские буквы: ");
+string text = Console.ReadLine();
+Console.WriteLine();
+PrintConsonants(text);
